e6abb0b1cf58ee535a03d48f70026530dd1ffc7d
Matt Yao <mattyao@b2cloud.com.au>
2016-01-27 17:06:20 +1100

    Added user model.

---
+++ models/user.js
@@ -0,0 +1,47 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+var userSchema = new mongoose.Schema({
+  first_name: {type: string, required: true},
+  last_name: {type: string, required: true},
+  email: {type: string, required: true},
+  password: {type: string, required: true},
+  ign: {type: string}
+});
+
+userSchema
+.virtual('full_name')
+.get(function(){
+  return this.first_name + ' ' + this.last_name;
+});
+
+userSchema.pre('save', function(cb){
+  var user = this;
+
+  if(!user.isModifed('password'))
+    return cb();
+
+  bcrypt.getSalt(5, function(err, salt){
+    if(err)
+      return cb(err);
+
+    bcrypt.hash(user.password, salt, null, function(err, hash){
+      if(err)
+        return cb(err);
+
+      user.password = hash;
+      cb();
+    });
+  });
+});
+
+userSchema.methods.verifyPasword = function(password, cb){
+  bcrypt.compare(password, this.password, function(err, isMatch){
+    if(err)
+      return cb(err);
+
+    cb(null, isMatch);
+  });
+};
+
+module.exports = mongoose.model('user', userSchema);
